7432f5d787269b4bdc509467d749aa276287cbba
basiagam <basiagam@yahoo.com>
2018-05-28 14:00:56 +0200

    routers and set up

---
+++ OdeToFood/OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class AboutController : Controller
+    {
+       public string Phone()
+        {
+            return "1+555+555+5555";
+        }
+        public string Address()
+        {
+            return "USA";
+        }
+    }
+}
