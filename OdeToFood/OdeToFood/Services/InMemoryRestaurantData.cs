1e30cad3bf35b097358416220ddc4300de7aa560
basiagam <basiagam@yahoo.com>
2018-05-30 08:58:04 +0200

    razor pages edit

---
+++ OdeToFood/OdeToFood/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,44 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    //public class InMemoryRestaurantData :IRestaurantData
+    //{
+    //    public InMemoryRestaurantData()
+    //    {
+    //        _restaurants = new List<Restaurant>
+    //        {
+    //            new Restaurant{ Id = 1, Name = "Trattoria"},
+    //            new Restaurant{ Id = 2, Name = "Urfa"},
+    //            new Restaurant{ Id=3, Name="Sevi"}
+    //        };
+    //    }
+    //    List<Restaurant> _restaurants;
+
+    //    public IEnumerable<Restaurant> getAll()
+    //    {
+    //        return _restaurants.OrderBy(r => r.Name);
+    //    }
+
+    //    public Restaurant Get(int id)
+    //    {
+    //        return _restaurants.FirstOrDefault(r => r.Id == id);
+    //    }
+
+    //    public Restaurant Add(Restaurant restaurant)
+    //    {
+    //        restaurant.Id = _restaurants.Max(r => r.Id) + 1;
+    //        _restaurants.Add(restaurant);
+    //        return restaurant;
+    //    }
+
+    //    public Restaurant Update(Restaurant restaurant)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
