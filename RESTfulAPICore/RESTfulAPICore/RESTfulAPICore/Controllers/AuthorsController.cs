d28d3df597b5754c0c6819e33f616ebbade1e875
basiagam <basiagam@yahoo.com>
2018-05-30 15:15:27 +0200

    api start

---
+++ RESTfulAPICore/RESTfulAPICore/RESTfulAPICore/Controllers/AuthorsController.cs
@@ -0,0 +1,26 @@
+﻿using RESTfulAPICore.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTfulAPICore.Controllers.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+
+    }
+}
---
+++ RESTfulAPICore/RESTfulAPICore/RESTfulAPICore/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RESTfulAPICore.Entities;
+using RESTfulAPICore.Services;
+
+namespace Rest
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // register the DbContext on the container, getting the connection string from
+            // appSettings (note: use this during development; in a production environment,
+            // it's better to store the connection string in an environment variable)
+            var connectionString = Configuration["connectionStrings:libraryDBConnectionString"];
+            services.AddDbContext<LibraryContext>(o => o.UseSqlServer(connectionString));
+
+            // register the repository
+            services.AddScoped<ILibraryRepository, LibraryRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+            /* SPRAWDZIC CONFIGURE LINIA PO LINII GDZIE SIE WYKRZACZA (NOTEPAD)*/
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            ILoggerFactory loggerFactory, LibraryContext libraryContext)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            libraryContext.EnsureSeedDataForContext();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
